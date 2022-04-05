2f606ccfce091502a4e385d9ea270ae6c51dc8b0
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-05 23:18:43 +0700

    modify student entity

--- src/main/java/id/ac/uin/student/entity/Student.java
+++ src/main/java/id/ac/uin/student/entity/Student.java
@@ -1,31 +1,49 @@
 package id.ac.uin.student.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "student")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
+    @NotEmpty(message = "First Name is required")
+    @NotNull(message = "FirstName can not be null!!")
     @Column(nullable = false, name = "first_Name")
     private String firstName;
 
-    @NotEmpty
+    @NotEmpty(message = "Last Name is required")
+    @NotNull(message = "LastName can not be null!!")
     @Column(nullable = false, name = "last_name")
     private String lastName;
 
-    @NotEmpty
+    @NotEmpty(message = "Email is required")
     @Column(name = "email", unique = true)
     private String email;
 
-    @NotEmpty
+    @NotNull(message = "Please enter birth date")
+    @Past(message = "Birth date should be less than current date!!")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDate;
+
+    @NotEmpty(message = "Phone is required")
+    @NotNull(message = "Phone can not be null!!")
+    @Size(max = 13, min = 10, message = "Mobile number should be of 10 digits")
     @Column(name = "phone_number", unique = true)
     private String phone;
 
