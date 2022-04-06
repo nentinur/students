594741ffd69eea5e86d4e5d37059e5908fac4d70
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-06 13:29:59 +0700

    memperbaiki error

--- src/main/java/id/ac/uin/student/controller/TestController.java
+++ src/main/java/id/ac/uin/student/controller/TestController.java
@@ -1,6 +1,7 @@
 package id.ac.uin.student.controller;
 
 import id.ac.uin.student.entity.Student;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
